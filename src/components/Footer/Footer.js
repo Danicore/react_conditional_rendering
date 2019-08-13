3a64eebaa8ab2e04f40dec0bb1d296a717e701c9
dani <groddpapyrus@yahoo.com>
2019-08-13 21:50:37 +0500

    conditional-rendering

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = (props) => {
+    return (
+        <footer className="Footer-container">Footer</footer>
+    )
+}
+
+export default Footer;
